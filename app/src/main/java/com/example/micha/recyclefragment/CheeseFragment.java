7879bf291d3afa52f98791cc9774817db078c006
Mazogu <michael_doom@hotmail.com>
2018-01-24 20:39:19 -0500

    Added custom layout for the adapter. Created inner RecyclerView class in the fragment

---
+++ app/src/main/java/com/example/micha/recyclefragment/CheeseFragment.java
@@ -0,0 +1,46 @@
+package com.example.micha.recyclefragment;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CheeseFragment extends Fragment {
+
+
+    public CheeseFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cheese, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private class CheeseView extends RecyclerView{
+
+        public CheeseView(Context context) {
+            super(context);
+        }
+    }
+
+
+
+}
